1548116e524bf0da80e7fb5b683837cc292a3351
Bill Ellis <bill.ellis@capgemini.com>
2024-08-27 08:50:53 +0100

    Alternative Haskel Cache build.

--- scripts/user-haskell.sh
+++ scripts/user-haskell.sh
@@ -9,19 +9,25 @@ set -exuo pipefail
 mkdir -p "/util"
 mkdir -p "/util/haskell"
 
-# Prepare.
-mkdir -p "/util/haskell/stack"
-chown "${USER_NAME}:${GROUP_NAME}" "/util/haskell/stack"
-
 # The Haskell Tool Stack dynamically pulls its dependencies at build time.
 # For expediency, we seek to pull, and retain, the likely needed dependencies
 # in advance.
+mkdir -p "/util/haskell/stack"
 mkdir -p "/util/haskell/init"
-chown "${USER_NAME}:${GROUP_NAME}" "/util/haskell/init"
-git clone "git@github.com:seL4/capdl.git" "/util/haskell/init/capdl"
-chown -R "${USER_NAME}:${GROUP_NAME}" "/util/haskell/init/capdl"
+cd "/util/haskell/init"
+/util/repo/repo init "git@github.com:seL4/camkes-manifest.git"
+/util/repo/repo sync
+mkdir "build"
+cd "build"
+python -m venv pyenv
+source pyenv/bin/activate
+pip install sel4-deps
+pip install camkes-deps
+pip install "protobuf<=3.20.*"
 export STACK_ROOT="/util/haskell/stack"
-sudo -u "${USER_NAME}" --preserve-env=STACK_ROOT make -C "/util/haskell/init/capdl/capDL-tool"
+../init-build.sh
+ninja
+chown --recursive "${USER_NAME}:${GROUP_NAME}" "/util/haskell/stack"
 rm -rf "/util/haskell/init"
 
 ################################################################################
